ded63ef19fd246213955ecb72a8d01e7899f55c5
ErElectrical <94623283+ErElectrical@users.noreply.github.com>
2021-12-20 19:03:01 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace stopwatch_stimulate
+{
+    public class timer
+    {
+        static void Main(string [] args)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            Thread.Sleep(1000);
+            timer.Stop();
+            //get elapsed time as a timespan
+            TimeSpan ts = timer.Elapsed;
+            string end_time = string.Format(0, ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            Console.WriteLine()
+
+
+
+
+        }
+    }
+}
