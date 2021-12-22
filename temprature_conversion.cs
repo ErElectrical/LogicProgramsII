f0d04736bdf174895ac8d10ed6fd5b16f00e630a
ErElectrical <94623283+ErElectrical@users.noreply.github.com>
2021-12-22 03:02:05 +0000

    Add files via upload

---
+++ temprature_conversion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace temprature
+{
+    public class program
+    {
+        public static void temprature_conversion(string unit, double temp)
+        {
+            if (unit == "celsius")
+            {
+                double farenhit = (temp * 9 / 5) + 32;
+                Console.WriteLine("conversion celsisus to farenhite ---- > " + farenhit);
+            }
+            else if (unit == "farenhit")
+            {
+                double celsius = (temp - 32) * 5 / 9;
+                Console.WriteLine("conversion farenhit to celsisis" + celsius);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your unit celsius or farenhit ");
+            string unit = Console.ReadLine();
+            Console.WriteLine("enter temprature ");
+            double temp=Convert.ToDouble(Console.ReadLine());
+            program.temprature_conversion(unit, temp);
+
+
+
+        }
+    }
+}
